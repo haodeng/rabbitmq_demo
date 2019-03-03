2dd7dfa48f3747f1cdc021b79e796c8f1d689976
hao <hao@neoconsult.dk>
2019-03-03 23:51:28 +0100

    simple queue

---
+++ src/main/java/simple_queue/Producer.java
@@ -0,0 +1,23 @@
+package simple_queue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer{
+    private final static String QUEUE_NAME = "simple_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            String message = "Hao demo, simple queue";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+
+            System.out.println(message + " -- sent.");
+        }
+    }
+}
