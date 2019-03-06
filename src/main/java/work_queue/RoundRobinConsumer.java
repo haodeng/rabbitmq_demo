35b46694c8edcc46db100b3790ec2b1ad6fc9bee
hao <hao@neoconsult.dk>
2019-03-06 01:08:11 +0100

    demo for work queue

---
+++ src/main/java/work_queue/RoundRobinConsumer.java
@@ -0,0 +1,35 @@
+package work_queue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class RoundRobinConsumer {
+    private static final String TASK_QUEUE_NAME = "work_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+
+            System.out.println("Received '" + message + "'");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException _ignored) {
+                Thread.currentThread().interrupt();
+            } finally {
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+    }
+
+}
